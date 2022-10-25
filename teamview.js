ecbd91506a8ced00708bbeb4f170c18781080635
hemannering <55000580+hemannering@users.noreply.github.com>
2022-10-24 22:25:04 -0400

    Add files via upload
    teamview

---
+++ teamview.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Text, View, Image, ImageBackground, TouchableOpacity, TextInput, StyleSheet, Button, Alert} from "react-native";
+
+
+export default function teamview(){
+  const styles = StyleSheet.create({
+    texttype: {fontSize:25 , fontFamily: 'Bungee-Regular', color: '#D9D9D9'},
+    profileText: {
+      position: 'absolute',
+      top: 190,
+      left: 150, 
+      right: 0,
+      bottom: 0,
+      justifyContent: 'flex-start',
+      },
+    profileTextNames: {
+        position: 'absolute',
+        top: 103,
+        left: 10, 
+        right: 0,
+        bottom: 500,
+        justifyContent: 'flex-start',
+      },
+  button3: {
+    //flex: 1,
+    alignItems: "center",
+    backgroundColor: "transparent",
+    padding: 0,
+    paddingHorizontal: 30,
+    justifyContent: 'center'
+
+    
+}, 
+Circle:
+{
+  width: 150,
+  height: 150,
+  borderRadius: 150 / 2,
+  backgroundColor: '#FF9800',
+}
+
+  });
+   return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+    <ImageBackground
+     style={{
+       //resizeMode: "center",
+       height: '100%',
+       width: '100%'
+     }
+    }
+     source={require("./profilebg.png")}
+     
+   />
+   
+    <View style= {[styles.Circle, {position: 'absolute'}]}> </View> 
+   <View style={{ position: 'absolute', top: 200, left: 50, height: 30, width: 30, resizeMode: 'stretch', alignItems:'center'}}>
+   <Image
+     source={require('./headshot3.png')}></Image>
+   </View>
+   <View style={styles.profileTextNames}>
+    <Text style={[styles.texttype, {fontSize: 30}]}>USERNAME</Text>
+    <Text style={[styles.texttype, {fontSize: 20, bottom:30}]}>Team Name</Text>
+   </View>
+   <View style={styles.profileText}>
+        <Text style={styles.texttype}>FIRST LAST</Text>
+        <Text style={[styles.texttype, {fontSize: 14}]}>Height: 6'4   Weight: 280 lbs</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Experience Level:</Text>
+        <Text style={[styles.texttype, {fontSize: 14}]}>Intermediate</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Position:</Text>
+        <Text style={[styles.texttype, {fontSize: 14}]}>power forward</Text>
+        </View>
+        <View style={{position: 'absolute', top: 0, left: 240, right: 0, bottom: 680, justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+            //inbox button, transparent
+            //onPress={() => console.log("button pressed!")} 
+            onPress={() =>Alert.alert("inbox")}
+            style={styles.button3}>
+        <Text style={{color: "transparent", fontSize: 28, fontFamily: 'Bungee-Regular'}}> </Text>
+        </TouchableOpacity>
+        </View>
+
+        <View style={{position: 'absolute', top: 0, left: 380, right: 0, bottom: 680, justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+            //profile button, transparent
+            //onPress={() => console.log("button pressed!")} 
+            onPress={() =>Alert.alert("profile")}
+            style={styles.button3}>
+        <Text style={{color: "transparent", fontSize: 28, fontFamily: 'Bungee-Regular'}}> </Text>
+        </TouchableOpacity>
+  
+        </View>
+  </View>
+   );
+}
