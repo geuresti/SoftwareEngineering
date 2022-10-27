6a4f8c2bb993693710be7b502ac4dcc76511ba30
hemannering <55000580+hemannering@users.noreply.github.com>
2022-10-26 22:08:46 -0400

    Update teamview.js
    to make team view be of players boxes created based on data which rn is hardcoded

--- teamview.js
+++ teamview.js
@@ -1,95 +1,73 @@
-import React from "react";
-import { Text, View, Image, ImageBackground, TouchableOpacity, TextInput, StyleSheet, Button, Alert} from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, ImageBackground, Dimensions } from 'react-native';
 
+const data = [
+  { key: 'Player' }, { key: 'Player' }, { key: 'Player' }, { key: 'Player' }, { key: 'Player' }, 
+  // { key: 'K' },
+  // { key: 'L' },
+];
 
-export default function teamview(){
-  const styles = StyleSheet.create({
-    texttype: {fontSize:25 , fontFamily: 'Bungee-Regular', color: '#D9D9D9'},
-    profileText: {
-      position: 'absolute',
-      top: 190,
-      left: 150, 
-      right: 0,
-      bottom: 0,
-      justifyContent: 'flex-start',
-      },
-    profileTextNames: {
-        position: 'absolute',
-        top: 103,
-        left: 10, 
-        right: 0,
-        bottom: 500,
-        justifyContent: 'flex-start',
-      },
-  button3: {
-    //flex: 1,
-    alignItems: "center",
-    backgroundColor: "transparent",
-    padding: 0,
-    paddingHorizontal: 30,
-    justifyContent: 'center'
+const formatData = (data, numColumns) => {
+  const numberRows = Math.floor(data.length / numColumns);
 
-    
-}, 
-Circle:
-{
-  width: 150,
-  height: 150,
-  borderRadius: 150 / 2,
-  backgroundColor: '#FF9800',
-}
+  let numberLastRow = data.length - (numberRows * numColumns);
+  while (numberLastRow !== numColumns && numberLastRow !== 0) {
+    data.push({ key: `blank-${numberLastRow}`, empty: true });
+    numberLastRow++;
+  }
 
-  });
-   return (
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-    <ImageBackground
-     style={{
-       //resizeMode: "center",
-       height: '100%',
-       width: '100%'
-     }
+  return data;
+};
+
+const numColumns = 3;
+export default class App extends React.Component {
+  renderItem = ({ item, index }) => {
+    if (item.empty === true) {
+      return <View style={[styles.item, styles.itemInvisible]} />;
     }
-     source={require("./profilebg.png")}
-     
-   />
-   
-    <View style= {[styles.Circle, {position: 'absolute'}]}> </View> 
-   <View style={{ position: 'absolute', top: 200, left: 50, height: 30, width: 30, resizeMode: 'stretch', alignItems:'center'}}>
-   <Image
-     source={require('./headshot3.png')}></Image>
-   </View>
-   <View style={styles.profileTextNames}>
-    <Text style={[styles.texttype, {fontSize: 30}]}>USERNAME</Text>
-    <Text style={[styles.texttype, {fontSize: 20, bottom:30}]}>Team Name</Text>
-   </View>
-   <View style={styles.profileText}>
-        <Text style={styles.texttype}>FIRST LAST</Text>
-        <Text style={[styles.texttype, {fontSize: 14}]}>Height: 6'4   Weight: 280 lbs</Text>
-        <Text style={[styles.texttype, {fontSize: 18}]}>Experience Level:</Text>
-        <Text style={[styles.texttype, {fontSize: 14}]}>Intermediate</Text>
-        <Text style={[styles.texttype, {fontSize: 18}]}>Position:</Text>
-        <Text style={[styles.texttype, {fontSize: 14}]}>power forward</Text>
-        </View>
-        <View style={{position: 'absolute', top: 0, left: 240, right: 0, bottom: 680, justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity
-            //inbox button, transparent
-            //onPress={() => console.log("button pressed!")} 
-            onPress={() =>Alert.alert("inbox")}
-            style={styles.button3}>
-        <Text style={{color: "transparent", fontSize: 28, fontFamily: 'Bungee-Regular'}}> </Text>
-        </TouchableOpacity>
-        </View>
+    return (
+      <View
+        style={styles.item}
+      >
+        <Text style={styles.itemText}>{item.key}</Text>
+      </View>
+    );
+  };
 
-        <View style={{position: 'absolute', top: 0, left: 380, right: 0, bottom: 680, justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity
-            //profile button, transparent
-            //onPress={() => console.log("button pressed!")} 
-            onPress={() =>Alert.alert("profile")}
-            style={styles.button3}>
-        <Text style={{color: "transparent", fontSize: 28, fontFamily: 'Bungee-Regular'}}> </Text>
-        </TouchableOpacity>
-  
-        </View>
-  </View>
-   );
+  render() {
+    return (
+        
+       
+      <FlatList
+        data={formatData(data, numColumns)}
+        style={styles.container}
+        renderItem={this.renderItem}
+        numColumns={numColumns}
+      />
+      
+     
+
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 20,
+  },
+  item: {
+    backgroundColor: '#4D243D',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    margin: 1,
+    height: Dimensions.get('window').width / numColumns, 
+  },
+  itemInvisible: {
+    backgroundColor: 'transparent',
+  },
+  itemText: {
+    color: '#fff',
+  },
+});
