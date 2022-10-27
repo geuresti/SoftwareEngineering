644ce3730c43d4aa219bf425f1fe76d344955817
cparker2310 <55166461+cparker2310@users.noreply.github.com>
2022-10-26 23:47:37 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
-import React from "react";
-import Login from "./login.js";
+import 'react-native-gesture-handler';
 
-export default function App(){
-   return (
-     <Login />
-   );
-}
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from "./LoginScreen.js"
+import UserList from "./UserList.js"
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{
+            title: 'Login', //Set Header Title
+            
+          }}
+        />
+
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={{
+            title: 'UserList', //Set Header Title
+
+          }}
+        />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
