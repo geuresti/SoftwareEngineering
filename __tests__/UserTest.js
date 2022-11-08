291bdbb3781266df481df3a76105327f9051e084
cparker2310 <55166461+cparker2310@users.noreply.github.com>
2022-11-08 18:30:29 -0500

    CRUD user testing

---
+++ __tests__/UserTest.js
@@ -0,0 +1,71 @@
+import UserDao from "../model/UserDao.js"
+import {expect, jest, test} from '@jest/globals'
+
+
+let userDao = null
+
+beforeEach(function(){
+    userDao = new UserDao()
+    userDao.deleteUser("test_user@gmail.com")
+
+});
+
+
+test('create user', () => {
+
+    let email = "test_user@gmail.com"
+    let password = "test_password"
+    let user = userDao.createUser(email, password)
+    expect(user.username).toEqual(email)
+    expect(user.pass).toEqual(password)
+
+    let user2 = userDao.readUser(email)
+
+    expect(user).toEqual(user2)
+  });
+
+test('read user with nonexistant email', () => {
+
+    let email = "notValid@email.com"
+    let user = userDao.readUser(email)
+    expect(user).toBe(null)
+    
+  });
+
+test('read user by email', () => {
+    let email = "test_user@gmail.com"
+    let password = "test_password"
+    
+    let created = userDao.createUser(email, password)
+    let read = userDao.readUser(email)
+    expect(created).toEqual(read)
+    
+  });
+
+  test('update user by email', () => {
+    let email = "test_user@gmail.com"
+    let password = "test_password"
+    
+    let created = userDao.createUser(email, password)
+    let read = userDao.readUser(email)
+
+    let updatedPassword = "updated_password"
+    let updated = userDao.updateUser(email, updatedPassword)
+    let read2 = userDao.readUser(email)
+
+    expect(read).not.toBe(read2)
+    expect(read2.pass).toBe(updatedPassword)
+    
+  });
+
+  test('delete user by email', () => {
+    let email = "test_user@gmail.com"
+    let password = "test_password"
+    let created = userDao.createUser(email, password)
+    
+
+    let deleted = userDao.deleteUser(email)
+    let read = userDao.readUser(email)
+    expect(read).toBe(null)
+    
+  });
