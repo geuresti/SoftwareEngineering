857197d35864c7ebd528a1576122c88f4fd062d4
hemannering <55000580+hemannering@users.noreply.github.com>
2022-11-08 20:53:56 -0500

    Adding Button for Update & Player Fields

--- ProfileView.js
+++ ProfileView.js
@@ -1,7 +1,11 @@
-import React from "react";
-import { Text, View, Image, ImageBackground, TouchableOpacity, TextInput, StyleSheet, Alert, navigation} from "react-native";
+import { Text, View, Image, ImageBackground, TouchableOpacity, TextInput, StyleSheet, Alert, SafeAreaView, FlatList, navigation} from "react-native";
 import PlayerDao from "./model/PlayerDao.js"
-
+import React, { useEffect, useState } from 'react';
+//import Mybutton from './pages/components/Mybutton';
+//import Mytext from './pages/components/Mytext';
+import { openDatabase } from 'react-native-sqlite-storage';
+import Realm from "realm";
+import dbModel from './dbModel';
 
 const ProfileView = ({ navigation }) => {
   const styles = StyleSheet.create({
@@ -22,6 +26,15 @@ const ProfileView = ({ navigation }) => {
         bottom: 500,
         justifyContent: 'flex-start',
       },
+      button2:{
+         //flex: 1,
+    alignItems: "center",
+    backgroundColor: "#ff6347",
+    padding: 0,
+    paddingHorizontal: 30,
+    justifyContent: 'center'
+
+      },
   button3: {
     //flex: 1,
     alignItems: "center",
@@ -38,6 +51,9 @@ const ProfileView = ({ navigation }) => {
   let playerDao = new PlayerDao()
   let curr = playerDao.getCurrentPlayer()
   let player = playerDao.readPlayer(curr.email)
+  // let updatedPlayer = playerDao.updatePlayer()
+  // let playerDelete = playerDao.deletePlayer(curr.deletePlayer)
+  
 
    return (
     <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
@@ -64,6 +80,23 @@ const ProfileView = ({ navigation }) => {
         <Text style={[styles.texttype, {fontSize: 14}]}>{player.experience}</Text>
         <Text style={[styles.texttype, {fontSize: 18}]}>Position:</Text>
         <Text style={[styles.texttype, {fontSize: 14}]}>{player.position}</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Is Manager:</Text>
+        <Text style={[styles.texttype, {fontSize:18}]}>{player.isManager}</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Average Points:</Text>
+        <Text style = {[styles.texttype, {fontSize:18}]}>{player.avgPoints}</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Average Blocks:</Text>
+        <Text style = {[styles.texttype, {fontSize:18}]}>{player.avgBlocks}</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Average Steals:</Text>
+        <Text style = {[styles.texttype, {fontSize:18}]}>{player.avgSteals}</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Average Assists:</Text>
+        <Text style = {[styles.texttype, {fontSize:18}]}>{player.assists}</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Free Throw Percent:</Text>
+        <Text style = {[styles.texttype, {fontSize:18}]}>{player.freethrowPrecent}</Text>
+        <Text style={[styles.texttype, {fontSize: 18}]}>Shot Percent:</Text>
+        <Text style = {[styles.texttype, {fontSize:18}]}>{player.shotPercent}</Text>
+
+
+        
         </View>
         <View style={{position: 'absolute', top: 0, left: 240, right: 0, bottom: 680, justifyContent: 'center', alignItems: 'center'}}>
         <TouchableOpacity
@@ -83,9 +116,25 @@ const ProfileView = ({ navigation }) => {
             style={styles.button3}>
         <Text style={{color: "transparent", fontSize: 28, fontFamily: 'Bungee-Regular'}}> </Text>
         </TouchableOpacity>
-  
         </View>
-  </View>
-   );
-}
+  
+      
+  
+
+        <View style={{position: 'absolute', top: 650, left: 200, right: 20, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+            //onPress={() => console.log("button pressed!")} 
+            // update user 
+            onPress={() => navigation.navigate('ProfileEdit')}
+            style={styles.button2}>
+        <Text style={{color: "#FFFFFF", fontFamily: 'monospace'}}> Update Player </Text>
+        </TouchableOpacity>
+        </View> 
+        </View> 
+        
+   )
+
+
+      
+};
 export default ProfileView
