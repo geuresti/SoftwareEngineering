da4491b951a71a2fff60313a48412d80d83650af
cparker2310 <55166461+cparker2310@users.noreply.github.com>
2022-11-08 21:23:04 -0500

    update with team dao

--- TeamList.js
+++ TeamList.js
@@ -1,46 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, Image, ImageBackground, TouchableOpacity, TextInput, StyleSheet, Button, Alert, SafeAreaView, FlatList} from "react-native";
-//import Mybutton from './pages/components/Mybutton';
-//import Mytext from './pages/components/Mytext';
-import { openDatabase } from 'react-native-sqlite-storage';
-import Realm from "realm";
-
-var db = openDatabase({ name: 'UserDatabase.db' });
-realm = new Realm({path: 'logins.realm',
-schema:[
-    {
-    name: "User",
-    properties: {
-        username: "string",
-        pass: "string",
-    },
-    primaryKey: "username",
-
-
-    },
-],
-
-});
-realm2 = new Realm({path: 'team.realm',
-schema:[
-    {
-    name: "Team",
-    properties: {
-        teamName: "string",
-        //teamids: "string",
-        teamids: "string",
-    },
-    primaryKey: "teamName",
-
-
-    },
-],
-schemaVersion:4
-});
+import TeamDao from './model/TeamDao.js'
 
 const TeamList = ({ navigation }) => {
-    let [teamname, setTeamName] = useState('');
-    let [teamID, setTeamID] = useState('');
+    let [teamName, setTeamName] = useState('');
 
     const styles = StyleSheet.create({
       textheader: {
@@ -72,35 +35,13 @@ const TeamList = ({ navigation }) => {
 
       });
 
+      let teamDao = new TeamDao()
+      const allTeams = teamDao.readAllTeams();
 
-            //let [flatListItems, setFlatListItems] = useState([]);
-            this.state = {
-              FlatListItems: [],
-            };
-    
-          /*
-          useEffect(() => {
-            db.transaction((tx) => {
-              tx.executeSql('SELECT * FROM user_table', [], (tx, results) => {
-                var temp = [];
-                for (let i = 0; i < results.rows.length; ++i)
-                  temp.push(results.rows.item(i));
-                setFlatListItems(temp);
-              });
-            });
-          }, []);
-          */
-    
-          //useEffect(() => {
-    
-            const tasks = realm2.objects("Team");
-            console.log(tasks);
-            //var temp = [];
-            console.log(tasks.length)
 
-            this.state = {
-              FlatListItems: tasks,
-            };
+      this.state = {
+        FlatListItems: allTeams,
+      };
     
       let listViewItemSeparator = () => {
         return (
@@ -115,10 +56,10 @@ const TeamList = ({ navigation }) => {
         <View
             key={item.user_id}
             style={{ backgroundColor: '#383434', marginTop: 20, padding: 30, borderRadius: 10 }}>
-        <Text style={styles.textheader}>teamids</Text>
-        <Text style={styles.textbottom}>{item.teamids}</Text>
-        <Text style={styles.textheader}>TeamName</Text>
+        <Text style={styles.textheader}>Team Name</Text>
         <Text style={styles.textbottom}>{item.teamName}</Text>
+        <Text style={styles.textheader}>Manager</Text>
+        <Text style={styles.textbottom}>{item.teamManager}</Text>
         
           
         </View>
@@ -126,18 +67,74 @@ const TeamList = ({ navigation }) => {
         };
       
         let deleteTeam = () => {
-          realm2.write(() => {
-            const teamDel = realm2.objectForPrimaryKey("Team", teamname);
-          //const testDel = realm.objects("User").filtered(`username = ${userEmail}`);
-          console.log(teamDel);
-          realm2.delete(teamDel)
-        });
-          //dao.deleteTeam(teamID)
-          Alert.alert(
-            'Success',
-            'You have Deleted Successfully',
-            [
-              {
+
+          if(teamDao.readTeam(teamName)){
+            teamDao.deleteUser(teamName)
+            alert(
+              'Success',
+              'You have Deleted Successfully',
+              [
+                {
+                  text: 'Ok',
+                  onPress: () => navigation.navigate('TeamList'),
+                },
+              ],
+              { cancelable: false }
+            );
+            
+          }
+          
+          else{
+            alert(
+              'Team does not exist',
+              [
+                {
+                  text: 'Ok',
+                  onPress: () => navigation.navigate('TeamList'),
+                },
+              ],
+              { cancelable: false }
+            );
+            
+          };
+          }
+
+          
+    
+        let updateTeam = () => {
+      
+          if (!teamName) {
+            alert('Please fill team name');
+            return;
+          }
+
+          let read = teamDao.readTeam(teamName)
+          
+          if(read){
+            teamDao.updateTeam(
+              teamName, read.teamManager, read.players, read.record, read.avgPoints, 
+              read.avgBlocks, read.avgSteals, read.avgAssists, read.freethrowPercent, read.shotPercent
+              )
+            
+              alert(
+              'Success',
+              'You have Updated Successfully',
+              [
+                {
+                  text: 'Ok',
+                  onPress: () => navigation.navigate('TeamList'),
+                },
+              ],
+              { cancelable: false }
+            );
+          
+          }
+        
+          else{
+            alert(
+              'Team does not exist',
+              [
+                {
                 text: 'Ok',
                 onPress: () => navigation.navigate('TeamList'),
               },
@@ -146,40 +143,7 @@ const TeamList = ({ navigation }) => {
           );
           
         };
-        
-    
-        let updateTeam = () => {
-          //console.log(teamID, teamname);
-      
-          if (!teamID) {
-            alert('Please fill team id');
-            return;
-          }
-          if (!teamname) {
-            alert('Please fill name');
-            return;
-          }
-      
-          //dao.updateTeam(teamID, teamname);
-          realm2.write(() => {
-            const teamUp = realm2.objectForPrimaryKey("Team", teamname);
-          //const testDel = realm.objects("User").filtered(`username = ${userEmail}`);
-          console.log(teamUp);
-          teamUp.teamName = teamname;
-          teamUp.teamids = teamID;
-          //testUp.pass = userPassword;
-        });
-          
-          Alert.alert('Success','User updated successfully',
-             [
-              {
-               text: 'Ok',
-               onPress: () => navigation.navigate('TeamList'),
-              },
-             ],
-              { cancelable: false }
-              );
-        };
+        }
 
         return (
           <SafeAreaView style={{ flex: 1 }}>
@@ -191,20 +155,6 @@ const TeamList = ({ navigation }) => {
                   keyExtractor={(item, index) => index.toString()}
                   renderItem={({ item }) => listItemView(item)}
                 />
-              
-
-              
-            <Text style={{fontSize:20 , fontFamily: 'monospace', color: 'white'}}>ID</Text>
-           <TextInput 
-            style = {styles.input} keyboardType="number-pad"
-           textAlign={'center'}
-            placeholder="ID"
-            placeholderTextColor="white" 
-            onChangeText={
-              (teamID) => setTeamID(teamID)
-            }
-            />
-        
         
         <Text style={{fontSize:20 , fontFamily: 'monospace', color: 'white'}}>Team Name</Text>
         <TextInput 
@@ -219,14 +169,12 @@ const TeamList = ({ navigation }) => {
         
       
           <TouchableOpacity
-            //onPress={() => console.log("button pressed!")} 
                 onPress={deleteTeam}
                 style={styles.button}>
                 <Text style={{color: "#FFFFFF", fontFamily: 'monospace'}}>Delete Team</Text>
               </TouchableOpacity>
 
               <TouchableOpacity
-            //onPress={() => console.log("button pressed!")} 
                 onPress={updateTeam}
                 style={[styles.button , {backgroundColor: "#CA5A37"}]}>
                 <Text style={{color: "#FFFFFF", fontFamily: 'monospace'}}>Update Team</Text>
