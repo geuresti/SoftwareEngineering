c2e3eb4a7ef4aa4be29087f20be2c679817a0e1d
hemannering <55000580+hemannering@users.noreply.github.com>
2022-11-10 15:37:34 -0500

    #16 adding player dao for UD

--- model/PlayerDao.js
+++ model/PlayerDao.js
@@ -77,7 +77,39 @@ export default class Player{
         return player
     }
 
-
+     /* update start */
+    updatePlayer(playerInput, f_new="", l_new, id_new="", h_new=0, w_new=0, p_new="", e_new="", m_new ="", points_new =0, blocks_new=0, steals_new =0,a_new=0, f_throw_per=0,s_new_percent=0){
+        player_realm.write(() => {  
+            let playerUpdated = player_realm.objectForPrimaryKey("Player", playerInput);
+            playerUpdated.first_name = f_new;
+            playerUpdated.last_name = l_new;
+            playerUpdated.team_id = id_new;
+            playerUpdated.height = h_new;
+            playerUpdated.weight = w_new;
+            playerUpdated.position = p_new;
+            playerUpdated.experience = e_new ;
+            playerUpdated.isManager = m_new;
+            playerUpdated.avgPoints = points_new;
+            playerUpdated.avgBlocks = blocks_new;
+            playerUpdated.avgSteals = steals_new;
+            playerUpdated.assists = a_new;
+            playerUpdated.freethrowPercent = f_throw_per;
+            playerUpdated.shotPercent = s_new_percent;
+    
+        });
+        return this.readPlayer(playerInput)
+    }
+  
+    deletePlayer(playerInput){
+        player_realm.write(() => {
+            let deletedPlayer = player_realm.objectForPrimaryKey("Player", playerInput);
+            player_realm.delete(deletedPlayer)
+            // return player 
+        
+    })
+    return this.readPlayer(playerInput)
+    }
+    
     // need to merge with hannah
     readAllPlayers(){
         const players = player_realm.objects("Player");
