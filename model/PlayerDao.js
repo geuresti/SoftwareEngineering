6a5adccf72b70489264e912405dac9790b712fbe
cparker2310 <55166461+cparker2310@users.noreply.github.com>
2022-11-12 21:49:39 -0500

    Update PlayerDao.js

--- model/PlayerDao.js
+++ model/PlayerDao.js
@@ -81,10 +81,10 @@ export default class Player{
      // calling this function overwrites a player's email, which prevents the curr.email value from working
    // updatePlayer(email, f_new="", l_new="", id_new="", h_new=0, w_new=0, p_new="", e_new="", m_new ="", points_new =0, blocks_new=0, steals_new =0,a_new=0, f_throw_per=0,s_new_percent=0){
    // updatePlayer(email=this.email, f_new=this.first_name, l_new="", id_new="", h_new=0, w_new=0, p_new="", e_new="", m_new ="", points_new =0, blocks_new=0, steals_new =0,a_new=0, f_throw_per=0,s_new_percent=0){
-    updatePlayer(f_new, l_new, id_new, h_new, w_new, p_new, e_new, m_new, points_new, blocks_new, steals_new, a_new, f_throw_per, s_new_percent) { 
+    updatePlayer(username, f_new, l_new, id_new, h_new, w_new, p_new, e_new, m_new, points_new, blocks_new, steals_new, a_new, f_throw_per, s_new_percent) { 
 
         player_realm.write(() => {  
-            let playerUpdated = player_realm.objectForPrimaryKey("Player", loggedInPlayer);
+            let playerUpdated = player_realm.objectForPrimaryKey("Player", username);
             /*
             if (email) {
                 console.log("EMAIL UPDATED");
@@ -151,46 +151,16 @@ export default class Player{
         });
         
         return this.readPlayer(loggedInPlayer)
-        /*
-        player_realm.write(() => {  
-            let playerUpdated = player_realm.objectForPrimaryKey("Player", loggedInPlayer);
-            playerUpdated.email = args;
-            playerUpdated.first_name = f_new;
-            playerUpdated.last_name = l_new;
-            playerUpdated.team_id = id_new;
-            playerUpdated.height = h_new;
-            playerUpdated.weight = w_new;
-            playerUpdated.position = p_new;
-            playerUpdated.experience = e_new ;
-            playerUpdated.isManager = m_new;
-            playerUpdated.avgPoints = points_new;
-            playerUpdated.avgBlocks = blocks_new;
-            playerUpdated.avgSteals = steals_new;
-            playerUpdated.assists = a_new;
-            playerUpdated.freethrowPercent = f_throw_per;
-            playerUpdated.shotPercent = s_new_percent;
-    
-        });
-        return this.readPlayer(loggedInPlayer)
-        */
     }
   
     deletePlayer(useremail){
         player_realm.write(() => {
             let deletedPlayer = player_realm.objectForPrimaryKey("Player", useremail);
-            player_realm.delete(deletedPlayer)
+            if(deletedPlayer) {player_realm.delete(deletedPlayer)}
             // return player 
         
     })
-    //return this.readPlayer(playerInput)
-    }
-    
-    setManager(username, isManager){
-        player_realm.write(() => {
-            let updated = player_realm.objectForPrimaryKey("Player", username);
-            updated.isManager = isManager
-        })
-        return this.readPlayer(username)
+        return this.readPlayer(useremail)
     }
     
     // need to merge with hannah
