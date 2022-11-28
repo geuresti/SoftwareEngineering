52c89f9e739a19960a565b840de86556eec7f775
hemannering <55000580+hemannering@users.noreply.github.com>
2022-11-28 16:26:13 -0500

    #18
    testing for 18

--- __tests__/MatchTest.js
+++ __tests__/MatchTest.js
@@ -6,56 +6,68 @@ let matchDao = null
 
 beforeEach(function(){
     matchDao = new MatchDao()
-    matchDao.deleteMatch("test_match")
-    matchDao.deleteMatch("test_match2")
+    // matchDao.deleteMatch(1)
+    // matchDao.deleteMatch(2)
 
 });
 
 test('create match', () => {
 
+    let away_team = "away_test"
+    let home_team = "home_test"
+    let time = "7"
     let match_id = 1
-    let match = teamDao.createTeam(match_id)
-    let match2 = teamDao.readTeam(match_id)
+    let match = matchDao.createMatch(away_team, home_team, time)
+    let match2 = matchDao.readMatch(match_id)
 
     expect(match).toEqual(match2)
   });
 
-  test('read team with nonexistant name', () => {
+  test('read match with nonexistant id', () => {
 
-    let match_id = 1
-    let match = teamDao.readTeam(match_id)
+    let match_id = 999
+    let match = matchDao.readMatch(match_id)
     expect(match).toBe(null)
     
   });
 
   test('read match by matchid', () => {
     let match_id = 1
-    let created = teamDao.createMatch(match_id)
-    let read = teamDao.readMatch(match_id)
+    let away_team = "away_test"
+    let home_team = "home_test"
+    let time = "7"
+    let created = matchDao.createMatch(away_team, home_team, time)
+    let read = matchDao.readMatch(match_id)
     expect(created).toEqual(read)
     
   });
 
   test('update match by id', () => {
     let match_id = 1
-    let created = teamDao.createMatch(match_id)
-    let read = teamDao.readTeam(match_id)
-    let updated = teamDao.updateTeam(match_id, 1, 2, 7, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    let read2 = teamDao.readTeam(match_id)
+    let away_team = "away_test"
+    let home_team = "home_test"
+    let time = "7"
+    let created = matchDao.createMatch(away_team, home_team, time)
+    let read = matchDao.readMatch(match_id)
+    let updated = matchDao.updateMatch(match_id, 1, 2, "7", 20, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    let read2 = matchDao.readMatch(match_id)
 
     expect(read).not.toBe(updated)
     expect(read2.away_team_score).toEqual(1)
     expect(read2.home_team_score).toEqual(2)
-    expect(read2.game_time).toEqual(7)
+    expect(read2.game_time).toEqual("7")
     expect(read2.h_team_blocks).toEqual(20)
     
   });
 
   test('get all matches', () =>{
     let match_id = 1
+    let away_team = "away_test"
+    let home_team = "home_test"
+    let time = "7"
     let match = matchDao.readAllMatches()
     let len = match.length
-    let m = matchDao.createMatch(match_id)
+    let m = matchDao.createMatch(away_team, home_team, time)
     let matches_after = matchDao.readAllMatches()
     expect(len+1).toBe(matches_after.length)
   
@@ -63,10 +75,13 @@ test('create match', () => {
 
   test('delete match by id', () => {
     let match_id = 1
-    let created = matchDao.createMatch(match_id)
+    let away_team = "away_test"
+    let home_team = "home_test"
+    let time = "7"
+    let created = matchDao.createMatch(away_team, home_team, time)
     
     let deleted = matchDao.deleteMatch(match_id)
-    let read = teamDao.readMatch(match_id)
+    let read = matchDao.readMatch(match_id)
     expect(read).toBe(null)
     
   });
