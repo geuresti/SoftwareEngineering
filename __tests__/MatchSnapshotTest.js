05e2380f13d1c42642adbea763dcc47170f5bf90
hemannering <55000580+hemannering@users.noreply.github.com>
2022-11-29 15:26:20 -0500

    #18 update
    for all matches screen snapshot

--- __tests__/MatchSnapshotTest.js
+++ __tests__/MatchSnapshotTest.js
@@ -1,9 +1,20 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import MatchEdit from "../MatchEdit";
 import MatchView from "../MatchView";
+import MatchCreate from "../CreateMatch";
 
-    it("Matches Snapshot", () => {
+    it("Matches View Snapshot", () => {
         const domTree = renderer.create(<MatchView />).toJSON();
         expect(domTree).toMatchSnapshot();
   });
   
+  it("Matches Edit Snapshot", () => {
+    const domTree = renderer.create(<MatchEdit />).toJSON();
+    expect(domTree).toMatchSnapshot();
+});
+
+it("Matches Create Snapshot", () => {
+    const domTree = renderer.create(<MatchCreate />).toJSON();
+    expect(domTree).toMatchSnapshot();
+});
